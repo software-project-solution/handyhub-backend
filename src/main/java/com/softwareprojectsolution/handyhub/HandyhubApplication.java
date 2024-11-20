8452babf2b2613dce6169e69fc5359817f056d41
hiran_xdoto <hiran@xdoto.io>
2024-11-20 23:27:13 +0530

    Create project

---
+++ src/main/java/com/softwareprojectsolution/handyhub/HandyhubApplication.java
@@ -0,0 +1,13 @@
+package com.softwareprojectsolution.handyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandyhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandyhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwareprojectsolution/handyhub/HandyhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwareprojectsolution.handyhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandyhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
